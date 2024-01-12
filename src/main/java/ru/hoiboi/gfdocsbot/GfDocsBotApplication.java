dfd17c8a27dff5c242a64fc9aef67e2c841e7bb1
hoiboi <gentledemeiz@gmail.com>
2024-01-12 23:54:31 +0300

    start project

---
+++ src/main/java/ru/hoiboi/gfdocsbot/GfDocsBotApplication.java
@@ -0,0 +1,13 @@
+package ru.hoiboi.gfdocsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfDocsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfDocsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hoiboi/gfdocsbot/GfDocsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hoiboi.gfdocsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfDocsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
